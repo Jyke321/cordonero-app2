9cf8af956fbbbb1dc787e171514ff0595d9cd463
Jyke321 <jyke321@gmail.com>
2021-11-19 22:56:27 -0500

    html progress, test not working

--- src/main/java/data/DataHandler.java
+++ src/main/java/data/DataHandler.java
@@ -7,6 +7,8 @@ package data;
 
 import com.google.gson.Gson;
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.safety.Safelist;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -92,7 +94,28 @@ public class DataHandler {
         dataBuffer = String.valueOf(stringBuffer);
     }
     private void parseToHTML() {
-        
+        StringBuilder stringBuffer = new StringBuilder();
+        stringBuffer.append("<!DOCTYPE html>" + "<html lang=\"en\">" +
+            "<style type=\"text/css\">" + "table, td {" +
+            "border: 1px solid #333;" + "}" + "thead, tfoot {" +
+            "background-color: #333;" + "color: #fff;" + "}" +
+            "</style>" + "<head>" + "<meta charset=\"UTF-8\">" +
+            "<title>saveData</title>" + "</head>" + "<body>" +
+            "<table>" + "<thead>" + "<tr>" + "<th scope=\"col\">Serial Number</th>" +
+            "<th scope=\"col\">Name</th>" + "<th scope=\"col\">Value</th>" +
+            "</tr>" + "</thead>" + " <tbody>");
+        for (int i = 0; i<getItemCount(); i++) {
+            stringBuffer.append(String.format("%s" +
+                "%s%s%s" + "%s%s%s" + "%s%s%s" + "%s%s%s" + "%s",
+                "<tr>", "<td>",list.get(i).getSerialNumber(),"</td>",
+                "<td>",list.get(i).getName(),"</td>",
+                "<td>",list.get(i).getMonetaryValue(),"</td>",
+                "<td hidden>",list.get(i).getValue(),"</td>", "</tr>"));
+        }
+        stringBuffer.append("</tbody>" + "</table>" +
+            "</body>" + "</html>");
+        Document doc = Jsoup.parse(String.valueOf(stringBuffer));
+        dataBuffer = doc.toString();
     }
     private void parseToJSON() {
          Gson gson = new Gson();
